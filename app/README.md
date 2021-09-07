70a406b7efa0cb052e12e4aafacfea3734d34e4a
Babatunde Adeyemi <badeyemi@baosystems.com>
2021-09-07 10:34:15 +0100

    Initial commit

---
+++ app/README.md
@@ -0,0 +1,9 @@
+# Pharmacy Stock Management application
+
+### Color Theme
+
+- [Material.io Theme](https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=212121&secondary.color=62A3BB)
+
+### Issues
+
+- Toolbar shows Activity name as title, even when the toolbar title is left empty or set to empty
---
+++ app/src/main/java/com/baosystems/icrc/pharmacystockmanagement/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.baosystems.icrc.pharmacystockmanagement;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // TODO: Figure out a way to not do this.
+        //  Removing the line without fixing the issue adds the activity name
+        //  to the left of the toolbar
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.more_options, menu);
+//        return true;
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            // TODO: Show the app settings UI
+            Log.d(this.getLocalClassName(), "Settings clicked");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
