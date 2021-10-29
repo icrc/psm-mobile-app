a746db612e4ad54c4f7eb98ead16b7e04ad5e2df
Babatunde Adeyemi <badeyemi@baosystems.com>
2021-10-29 13:48:08 +0100

    refactor

--- app/src/main/java/com/baosystems/icrc/psm/utils/AttributeHelper.kt
+++ app/src/main/java/com/baosystems/icrc/psm/utils/AttributeHelper.kt
@@ -3,14 +3,11 @@ package com.baosystems.icrc.psm.utils
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance
 
 object AttributeHelper {
-    fun teiItemName(trackedEntityInstance: TrackedEntityInstance): String? =
-        trackedEntityInstance.trackedEntityAttributeValues()?.get(0)?.value()
+    fun teiAttributeValueAtIndex(trackedEntityInstance: TrackedEntityInstance, index: Int):
+            String? = trackedEntityInstance.trackedEntityAttributeValues()?.get(index)?.value()
 
-    fun teiItemCode(trackedEntityInstance: TrackedEntityInstance) =
-        trackedEntityInstance.trackedEntityAttributeValues()?.get(1)?.value()
-
-    fun teiItemValueByAttributeUid(trackedEntityInstance: TrackedEntityInstance,
-                                   attributeUid: String): String? {
+    fun teiAttributeValueByAttributeUid(trackedEntityInstance: TrackedEntityInstance,
+                                        attributeUid: String): String? {
         val attrValues = trackedEntityInstance.trackedEntityAttributeValues()
 
         return if (attrValues == null || attrValues.isEmpty()) {
