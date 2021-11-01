6dc08f8e0c3c66df1ad2708111e3c6b490596939
Babatunde Adeyemi <badeyemi@baosystems.com>
2021-11-01 15:16:27 +0100

    hide distributed to if not available

--- app/src/main/java/com/baosystems/icrc/psm/utils/Extensions.kt
+++ app/src/main/java/com/baosystems/icrc/psm/utils/Extensions.kt
@@ -1,6 +1,8 @@
 package com.baosystems.icrc.psm.utils
 
+import android.view.View
 import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.baosystems.icrc.psm.utils.Constants.LAST_SYNCED_DATETIME_FORMAT
 import com.baosystems.icrc.psm.utils.Constants.TRANSACTION_DATE_FORMAT
@@ -17,3 +19,13 @@ fun LocalDateTime.humanReadableDate(): String =
 @BindingAdapter("date")
 fun setDate(view: AutoCompleteTextView, date: LocalDateTime) =
     view.setText(date.humanReadableDate())
+
+@BindingAdapter("distributedTo")
+fun setDistributedTo(view: TextView, s: String?) {
+    if (s == null) {
+        view.visibility = View.GONE
+    } else {
+        view.text = s
+        view.visibility = View.VISIBLE
+    }
+}
